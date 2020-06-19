3630e2811c3b0ab9ca07275f9721085267165516
Abtahee Ali <abtaheeali@gmail.com>
2020-06-19 18:47:29 -0400

    Added a button to end player one's turn

--- public/main.js
+++ public/main.js
@@ -13,11 +13,19 @@ function handlePlayerOnePaperSelection(event) {
     'https://rpsls.net/images/paper.png'
 }
 
+function updateButton(event) {
+  const playerOneHidden = document.querySelector('.playerOneChoice')
+  playerOneHidden.style.display = 'none'
+}
+
 const main = () => {
   const playerOneRock = document.querySelector('.playerOneChoice .rock')
   playerOneRock.addEventListener('click', handlePlayerOneRockSelection)
 
   const playerOnePaper = document.querySelector('.playerOneChoice .paper')
   playerOnePaper.addEventListener('click', handlePlayerOnePaperSelection)
+
+  const button = document.querySelector('.endButton button')
+  button.addEventListener('click', updateButton)
 }
 document.addEventListener('DOMContentLoaded', main)
