e55462acad89550b6c0a85b065e38774ec999b66
Abtahee Ali <abtaheeali@gmail.com>
2020-06-19 16:38:19 -0400

    added button for rock

--- public/main.js
+++ public/main.js
@@ -1,7 +1,20 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let playerOne = 0
+let playerTwo = 0
+let playerOneChoice = ''
+let playerTwoChoice = ''
+
+function handlePlayerOneRockSelection(event) {
+  const choice = document.createElement('img')
+  choice.src = 'https://rpsls.net/images/rock.png'
+  choice.width = '200'
+  choice.height = '200'
+  // const addImg = document.querySelector('.figureOne')
+  document.body.appendChild(choice)
 }
 
+const main = () => {
+  const playerOneRock = document.querySelector('.playerOneChoice .rock')
+
+  playerOneRock.addEventListener('click', handlePlayerOneRockSelection)
+}
 document.addEventListener('DOMContentLoaded', main)
