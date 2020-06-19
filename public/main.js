6d122e05a2f44e27959eb3ccf5e29b6405518a0c
Abtahee Ali <abtaheeali@gmail.com>
2020-06-19 18:20:07 -0400

    when paper option is selected, paper image appears

--- public/main.js
+++ public/main.js
@@ -4,13 +4,20 @@ let playerOneChoice = ''
 let playerTwoChoice = ''
 
 function handlePlayerOneRockSelection(event) {
-  document.querySelector('.playerOneChoice .blank').src =
+  document.querySelector('.playerOneChoice .blankImage').src =
     'https://rpsls.net/images/rock.png'
+}
+
+function handlePlayerOnePaperSelection(event) {
+  document.querySelector('.playerOneChoice .blankImage').src =
+    'https://rpsls.net/images/paper.png'
 }
 
 const main = () => {
   const playerOneRock = document.querySelector('.playerOneChoice .rock')
-
   playerOneRock.addEventListener('click', handlePlayerOneRockSelection)
+
+  const playerOnePaper = document.querySelector('.playerOneChoice .paper')
+  playerOnePaper.addEventListener('click', handlePlayerOnePaperSelection)
 }
 document.addEventListener('DOMContentLoaded', main)
